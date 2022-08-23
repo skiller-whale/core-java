b2cfdb6d592c83b7db609b29a9279896bc93ed4e
Dave Millican <dave@skillerwhale.com>
2022-08-23 23:30:10 +0100

    Add comment

--- exercises/collections_and_generics/CommandLineSorter.java
+++ exercises/collections_and_generics/CommandLineSorter.java
@@ -3,7 +3,6 @@ import java.util.*;
 @SuppressWarnings("unchecked") // We'll explain this soon
 public class CommandLineSorter {
     public static void main(String[] args) {
-        //
+        // Your code here...
     }
 }
-
