004574fe1711a20193a23e333e2d5800b3bdf788
Matthew Bloch <matthew@bloch.tv>
2023-09-19 00:39:43 +0100

    Synchronisation with newest slides.

--- exercises/object_monitor/exercise_4/MyBlockingQueue.java
+++ exercises/object_monitor/exercise_4/MyBlockingQueue.java
@@ -4,23 +4,14 @@ import java.util.function.BooleanSupplier;
 class MyBlockingQueue<T> {
     static final int SIZE = 100; // Limit queue size
     final Queue<T> items  = new LinkedList<T>();
-    private boolean notEmpty() { return items.size() != 0; };
-    private boolean notFull() { return items.size() != SIZE; };
-
-    private void myWait(BooleanSupplier p) {
-        while (!p.getAsBoolean()) {
-            // spin-wait
-            continue;
-        }
-    }
 
     T pop() throws InterruptedException {
-        myWait(this::notEmpty);
+        while (items.size() == 0) { /* spin-wait */}
         return items.poll();
     }
 
     void push(T item) throws InterruptedException {
-        myWait(this::notFull);
+        while (items.size() == SIZE) { /* spin-wait */}
         items.offer(item);
     }
 }
