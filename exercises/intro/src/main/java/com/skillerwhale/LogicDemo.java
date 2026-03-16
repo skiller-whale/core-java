c2fce45048b9e4c25c870e5456d4a6c1a6217232
Kay Lack <kay@kaylack.net>
2026-03-16 19:34:56 +0000

    Redraft intro exercises

---
+++ exercises/intro/src/main/java/com/skillerwhale/LogicDemo.java
@@ -0,0 +1,51 @@
+package com.skillerwhale;
+
+public class LogicDemo {
+  public static void main(String[] args) {
+    String pickedNumString = IO.readln("Guess my number: ");
+
+    int pickedNum = Integer.parseInt(pickedNumString);
+
+    IO.println("You guessed: " + pickedNum);
+  }
+}
+
+// First, run this code to see it work.
+
+// Use this command:
+//
+//   cd intro
+//   ./run_logic_demo.sh
+
+// You now have a choice of exercises from easier to harder:
+
+// Level 1: Make this program print
+//          "You guessed my number!" if the user guesses 42,
+//          and "You guessed wrong!" otherwise.
+
+// Level 2: Make the program print
+//          "You guessed too low!" if the user guesses a number less than 42,
+//          "You guessed too high!" if the user guesses a number greater than 42,
+//          and "You guessed my number!" if the user guesses 42.
+
+// Level 3: Implement Level 2, but allow the user to keep guessing until they
+//          guess the number correctly.
+//
+//          You will need a `while` loop for this. It looks like this:
+//
+//          while (condition) {
+//            // code to run while condition is true
+//          }
+
+// Level 4: Implement level 3, but give the user a maximum of 5 guesses.
+
+// Level 5: Implement level 4, but also handle the situation where the user
+//          enters a non-numeric value.
+//
+//          You will need to use a `try`/`catch` block for this. It looks like this:
+//
+//          try {
+//            // code that might throw an exception
+//          } catch (ExceptionType e) {
+//            // code to run if an exception of type ExceptionType is thrown
+//          }
--- exercises/intro/src/main/java/com/skillerwhale/StyleDemo.java
+++ exercises/intro/src/main/java/com/skillerwhale/StyleDemo.java
@@ -2,7 +2,7 @@ package com.skillerwhale;
 
 import java.util.List;
 
-public class ExploreStyle {
+public class StyleDemo {
   public static void main() {
     List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     int total = 0;
