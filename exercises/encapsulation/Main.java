929644975944484dea663e1dee885b5195a6bb0c
traikodinev <3115248+traikodinev@users.noreply.github.com>
2026-04-02 13:57:31 +0000

    Sync exercise files

---
+++ exercises/encapsulation/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // fishy names
+        var alice = new User(0, "Al", "alice.smith@example.com");
+        var john = new User(1, "John Roe", "john.roe@example.com");
+    }
+}
